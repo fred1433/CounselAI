90d40fcd778b577ce87accd0d95890d7bb7133b4
Fred <frederic.de.choulot@gmail.com>
2025-06-18 17:29:32 -0500

    fix(frontend): configure proxy to fix 404 errors on API calls

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,7 +3,15 @@ const nextConfig = {
     output: 'standalone',
     typescript: {
         ignoreBuildErrors: true,
-    }
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://backend:3001/api/:path*',
+            },
+        ];
+    },
 };
 
 export default nextConfig; 
